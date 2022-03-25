5e88e288fcabb4022c8741ba8131dc4c3c8a7846
miladalinia <miladaliniya01@gmail.com>
2022-03-25 15:08:00 +0430

    navbar items changed

--- components/navigation/header.tsx
+++ components/navigation/header.tsx
@@ -6,7 +6,7 @@ const Header: NextPage = () => {
         <>
             <header>
                 <div
-                    className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 border-bottom box-shadow border-light">
+                    className="d-flex flex-column flex-md-row justify-content-between align-items-center p-3 px-md-4 mb-3 border-bottom box-shadow border-light">
                     <h5 className="my-0 mr-md-auto font-weight-normal">Hokusai</h5>
                     <nav className="my-2 my-md-0 mr-md-3">
                         <Link href="/">
